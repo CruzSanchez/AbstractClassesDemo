504955c48e4b33c63cbfc094f3a749e664cf123a
Elijah Heath <ejheath8888@gmail.com>
2022-02-28 12:40:17 -0500

    working with abstract classes

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,28 +10,15 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            /*
-             * Todo follow all comments!! 
-             */
+
 
             #region Vehicles
 
-            /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties Year, Make, and Model
-             * Set the defaults to something generic in the Vehicle class
-             * Vehicle shall have an abstract method called DriveAbstract with no implementation
-             * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
-             */
 
-            /* 
-             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-             * Provide the implementations for the abstract methods
-             * Only in the Motorcycle class will you override the virtual drive method
-            */
 
             // Create a list of Vehicle called vehicles
+
+            var vehicles = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
@@ -39,10 +26,28 @@ namespace ConsoleUI
              * Set the properties with object initializer syntax
              */
 
+            Car nissan = new Car() { HasSunroof = false, Make = "Nissan", Model = "Altima", Year = 2019 };
+            Motorcycle coolFriend = new Motorcycle() { NumberOfWheels = 3, Make = "European", Model = "Vintage", Year = 1985 };
+            Vehicle explorer = new Car() { HasSunroof = true, Make = "Ford", Model = "Explorer", Year = 2010 };
+            Vehicle coolFriend2 = new Motorcycle() { NumberOfWheels = 2, Make = "European2", Model = "Vintage2" };
+
+
+
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
+
+            vehicles.Add(nissan);
+            vehicles.Add(explorer);
+            vehicles.Add(coolFriend);
+            vehicles.Add(coolFriend2);
+
+            foreach(var vehicle in vehicles)
+            {
+                Console.WriteLine($"Make {vehicle.Make}, Model {vehicle.Model}, Year: {veh.Year}");
+                vehicle.DriveAbstract();
+            }
 
             // Call each of the drive methods for one car and one motorcycle
 
---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public  int Year { get; set; } = 1;
+        public  string Make { get; set; } = "Toyota";
+        public string Model { get; set; } = "Camry";
+
+        public abstract void DriveAbstract();
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This {Make} {Model} is virtually operating.");
+        }
+
+
+    }
+}
